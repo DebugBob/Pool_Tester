2ac837cb86e2b2f76a282031f9b88830de3ce42b
DebugBob <rastorino@outlook.com.au>
2026-01-13 19:31:44 +1100

    form mobile breakpoints

--- src/Pages/Tests.tsx
+++ src/Pages/Tests.tsx
@@ -5,7 +5,7 @@ const Tests = () => {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <main className="flex-1 py-20 flex items-center justify-center">
+      <main className="flex-1 pt-50 pb-10 md:pb-0 md:pt-0 flex items-center justify-center overflow-y-auto">
         <CustomerForm />
       </main>
     </div>
